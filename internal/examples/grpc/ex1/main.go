d02483b7e1667fda28d748c532bb564651e5ccbd
Paul Sitoh <2133987+paulwizviz@users.noreply.github.com>
2025-01-07 08:53:56 +0000

    Protoc wrapper (#3)
    Add image build scripts
    * Add docker-compose build script for protoc image
    
    Implement protoc container
    * Add function call to perform proto compilation
    
    Add example
    * Include example demonstrating the use of grpc package.
    
    Update documentation
    * Add reference to grpc package to README
    
    ---------
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

---
+++ internal/examples/grpc/ex1/main.go
@@ -0,0 +1,68 @@
+// Copyright 2025 The Contributors to narwhal
+// This file is part of the narwhal project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+//
+// For a list of contributors, refer to the CONTRIBUTORS file or the
+// repository's commit history.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/paulwizviz/narwhal/grpc"
+)
+
+func main() {
+
+	// STEP 1: Specify location of protos file and compiled artefacts
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	protoPath := filepath.Join(pwd, "testdata", "protos")
+	protoFile := "person.proto"
+	outPath := filepath.Join(pwd, "tmp", "go", "pkg")
+	if _, err := os.Stat(outPath); errors.Is(err, os.ErrNotExist) {
+		if err := os.MkdirAll(outPath, 0755); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// STEP 3: Instantiate Etherreum tool
+	tool, err := grpc.NewDefaultTool()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// STEP 4: Exxecute function to compile solidity
+	containerID, err := tool.CompileProtosGo(context.Background(), "narwhal/protoc:current", "grpc_container", protoPath, outPath, protoFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(containerID)
+
+	// STEP 5: Remove solidity compiler container
+	if err := tool.RemoveContainerForce(context.TODO(), containerID); err != nil {
+		log.Fatal(err)
+	}
+
+}
