50629800d1530a14369307298a4a6026685096ed
Paul Sitoh <paulwizviz@gmail.com>
2025-02-18 19:57:45 +0000

    Update grpc package
    - Add catch error copying to standard out.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -108,7 +108,10 @@ func compileProtosGo(ctx context.Context, client *dockersdk.Client, image string
 	}
 	defer out.Close()
 
-	io.Copy(os.Stdout, out)
+	_, err = io.Copy(os.Stdout, out)
+	if err != nil {
+		return "", err
+	}
 
 	return resp.ID, nil
 }
@@ -173,7 +176,10 @@ func compileProtosGRPC(ctx context.Context, client *dockersdk.Client, image stri
 	}
 	defer out.Close()
 
-	io.Copy(os.Stdout, out)
+	_, err = io.Copy(os.Stdout, out)
+	if err != nil {
+		return "", err
+	}
 
 	return resp.ID, nil
 }
